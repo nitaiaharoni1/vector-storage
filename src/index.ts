d53f06afba6398e7897f1d85facd6c93406dcbd5
naharoni <naharoni@fireblocks.com>
2023-05-22 14:23:19 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { VectorStorage } from './VectorStorage';
 export type { IVSOptions } from './types/IVSOptions';
-export type { IVSDocument } from './types/IVSDocument';
+export type { IVSDocument, IVSSimilaritySearchItem } from './types/IVSDocument';
 export type { IVSFilterOptions } from './types/IVSFilterOptions';
 export type { IVSSimilaritySearchParams } from './types/IVSSimilaritySearchParams';
