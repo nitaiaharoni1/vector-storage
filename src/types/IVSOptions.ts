f217c5f2c31e68b814644e310c22d92a58429e3e
Daniel Swiecki <daniel.swiecki@gmail.com>
2023-06-15 14:09:08 -0400

    UPDATED: Added support for Azure and API key
    - Added properties `azureProxy`, `azureEndpoint`, `azureApiKey`, and `openAIEndpoint` to the `VectorStorage` class
    - Modified the constructor to accept these new options
    - UpdatedembedTexts` function to use the Azure endpoint and API key if provided, or fallback to the OpenAI API key
    - Adjusted the code to handle the different scenarios for API key and endpoint
    - Updated the `IVSOptions` interface to include the new options

--- src/types/IVSOptions.ts
+++ src/types/IVSOptions.ts
@@ -1,4 +1,8 @@
 export interface IVSOptions {
+  azureProxy?: string; // Azure endpoint which manages your api key
+  azureEndpoint?: string; // Azure endpoint
+  azureApiKey?: string; // Azure api key
+  openAIEndpoint?: string; // The OpenAI API key used for generating embeddings.
   openAIApiKey?: string; // The OpenAI API key used for generating embeddings.
   maxSizeInMB?: number; // The maximum size of the storage in megabytes. Defaults to 4.8. Cannot exceed 5.
   debounceTime?: number; // The debounce time in milliseconds for saving to local storage. Defaults to 0.
